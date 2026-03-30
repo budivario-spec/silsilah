fe537ad2a4eb259161a2f1181491fef1434848a3
OmBudi78 <masbodong933@gmail.com>
2026-03-30 08:42:54 +0700

    Update: jumlah anggota lebih rinci masih hidup atau sudah wafat. Kabar Terbaru & Promo bisa diisi sendiri pakai menu Kabar Terbaru & Promo

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-const URL_API = "https://script.google.com/macros/s/AKfycbyXRD28yFtUZVftaCVYY6bcncDPSqO1G4uG3NiaiGRvfAX7jIrRIpxUPyzQJw89o4waIQ/exec";
+const URL_API = "https://script.google.com/macros/s/AKfycbyXMWajNjhyWkqSQb3_uNFdApKmk05Avp3BayOmUPH2i8-OEYiUBC0v_a3qLIZTTmEIjg/exec";
+const IMGBB_API_KEY = "7a50cf9038af8670089c3f94713e8678";
